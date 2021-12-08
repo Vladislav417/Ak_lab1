95d9fddc20e6918c877bd3d8b55b457d458139d5
Vladislav417 <66624087+Vladislav417@users.noreply.github.com>
2021-12-08 19:33:39 +0200

    Add files via upload

---
+++ calculator.h
@@ -0,0 +1,34 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator
+{
+/* printf("there"); */
+/* printf("there"); */
+/* printf("there"); */
+/* printf("there"); */
+/* printf("there"); */
+/* printf("there"); */
+/* printf("there"); */
+/* printf("there"); */
+/* printf("there"); */
+/* printf("there"); */
+/* printf("there"); */
+/* printf("there"); */
+/* printf("there"); */
+/* printf("there"); */
+/* printf("there"); */
+/* printf("there"); */
+/* printf("there"); */
+/* printf("there"); */
+/* printf("there"); */
+/* printf("there"); */
+/* printf("there"); */
+/* printf("there"); */
+/* printf("there"); */
+    public:
+        int Add (double, double);
+        int Sub (double, double);
+};
+
+#endif//CALCULATOR_H
